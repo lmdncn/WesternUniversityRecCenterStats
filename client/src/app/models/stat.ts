b108bd993f902db16a7a50885f66e14a2ba3c683
Liam Duncan <ldunca7@uwo.ca>
2017-01-13 20:25:05 -0500

    started stat service

---
+++ client/src/app/models/stat.ts
@@ -0,0 +1,22 @@
+//This defines a Tab object
+
+
+export class Stat {
+    _id: string;
+    loc: string;
+    count: number;
+    date: Date;
+  
+
+    constructor(_id:string, loc: string, count:number, date:Date) {
+        this.loc = loc;
+        this.count = count;
+        this._id = _id;
+        this.date = date;
+    }
+
+    
+
+
+
+}
--- client/src/app/services/stat.service.ts
+++ client/src/app/services/stat.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {Stat} from '../models/stat';
 @Injectable()
 export class StatService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getToday(loc:String): Observable<Stat[]> {
+
+    return this.http.get("api/stats/today")
+      .map((res) => res.json())
+      .catch(this.handleError);
+  }
+
+
+  private handleError(error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+
+    // console.log("Error with tab retrieval");
+
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 
 }
+
