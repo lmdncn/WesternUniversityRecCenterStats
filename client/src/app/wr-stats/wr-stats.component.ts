6ef3e1a66b316924de220ced51620b2b72aaa1ab
Liam Duncan <ldunca7@uwo.ca>
2017-01-17 02:35:32 -0500

    closure graphing

--- client/src/app/wr-stats/wr-stats.component.ts
+++ client/src/app/wr-stats/wr-stats.component.ts
@@ -32,20 +32,41 @@ export class WrStatsComponent implements OnInit {
   setData() {
 
     var TW = new Array<XY>();
+    var lastMoment = moment(this.thisWeekStats[0].date)
 
     for (var i = 0; i < this.thisWeekStats.length; i++) {
       console.log("This Week: ", this.thisWeekStats[i]);
+
+          while(moment(this.thisWeekStats[i].date)>lastMoment.add(2, 'hours'))//Gym Probably Closed
+      {
+        console.log("Adding Close");
+        TW.push( new XY(new Date(lastMoment.toDate()),-1));
+      }
+
+      lastMoment = moment(this.thisWeekStats[i].date);
+
+
       var t = new XY(new Date(this.thisWeekStats[i].date), this.thisWeekStats[i].count);
       TW.push(t);
     };
 
 
     var LW = new Array<XY>();
+    var lastMoment = moment(this.lastWeekStats[0].date)
 
     for (var i = 0; i < this.lastWeekStats.length; i++) {
+
       console.log("Last Week: ", this.lastWeekStats[i]);
-      var t = new XY(new Date(moment(this.lastWeekStats[i].date).add(7, "days").toDate()), this.lastWeekStats[i].count);
-      LW.push(t);
+
+      while(moment(this.lastWeekStats[i].date).add(7, "days")>lastMoment.add(2, 'hours'))//Gym Probably Closed
+      {
+        LW.push( new XY(new Date(lastMoment.toDate()),-1));
+      }
+
+      lastMoment = moment(this.lastWeekStats[i].date).add(7, "days");
+
+      LW.push( new XY(new Date(moment(this.lastWeekStats[i].date).add(7, "days").toDate()), this.lastWeekStats[i].count));
+      
     };
 
 
@@ -53,12 +74,18 @@ export class WrStatsComponent implements OnInit {
       datasets: [{
         label: 'This Week',
         data: TW,
-        backgroundColor: "rgba(81, 44, 115,0.8)"
+        backgroundColor: "rgba(81, 44, 115,0.8)",
+        lineTension:0.2,
+        radius:4,
+        spanGaps:false,
       },
       {
         label: 'Last Week',
         data: LW,
-        backgroundColor: "rgba(93, 90, 96,0.2)"
+        backgroundColor: "rgba(93, 90, 96,0.2)",
+        lineTension:0.2,
+        radius:4,
+        spanGaps:false,
       }
 
       ]
@@ -75,13 +102,20 @@ export class WrStatsComponent implements OnInit {
               day: 'dddd, MMM D',
             },
             isoWeekday: true,
-            max: moment(this.lastWeekStats[this.lastWeekStats.length - 1].date).add(7, "days"),
-            min: moment(this.thisWeekStats[0].date),
+
+           // max: moment(this.lastWeekStats[this.lastWeekStats.length - 1].date).add(7, "days"),
+           // min: moment(this.thisWeekStats[0].date),
+
+           max: moment().add(4, "days"),
+           min: moment().subtract(3,"days"),
+
             tooltipFormat: "ddd, MMM D, h:mm a",
             unit: "day"
           },
           ticks: {
-            labelOffset: 75,
+            labelOffset: 80,
+            maxRotation: 0,
+
           },
           gridLines: {
             lineWidth: 4,
@@ -102,6 +136,8 @@ export class WrStatsComponent implements OnInit {
 
       },
       responsive: true,
+      
+      
     };
 
   }
