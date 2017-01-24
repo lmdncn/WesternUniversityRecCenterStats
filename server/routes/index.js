2eaecb2a86ad56d56e2c1f6cd9b0cc0f3bf2e171
Liam Duncan <ldunca7@uwo.ca>
2017-01-24 09:12:37 -0500

    server update

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,81 +19,65 @@ setInterval(function() {
 };
 
 timeout();
+updater(true); //Force an update on server turn on
 
 // Get Home Page
 router.get('/', function (req, res, next) {
 
-	var td = Date.now();
-	if( moment(lastUpdate).add(2,"minutes") < moment(td))
-	{
-	lastUpdate = td;
-	updater.updateData();
-	}
+	updater();
     res.render('index.html');
 
 });
 
 router.get('/WeightRoom', function (req, res, next) {
 
-	var td = Date.now();
-	if( moment(lastUpdate).add(2,"minutes") < moment(td))
-	{
-	lastUpdate = td;
-	updater.updateData();
-	}
+	updater();
     res.render('index.html');
 	
 });
 
 router.get('/CardioMachines', function (req, res, next) {
 	
-	updater.updateData();
+	updater();
     res.render('index.html');
 
 });
 router.get('/Basketball', function (req, res, next) {
 
-	var td = Date.now();
-	if( moment(lastUpdate).add(2,"minutes") < moment(td))
-	{
-	lastUpdate = td;
-	updater.updateData();
-	}
+	updater();
     res.render('index.html');
 
 });
 router.get('/Volleyball', function (req, res, next) {
 
-	var td = Date.now();
-	if( moment(lastUpdate).add(2,"minutes") < moment(td))
-	{
-	lastUpdate = td;
-	updater.updateData();
-	}
+	updater();
     res.render('index.html');
 
 });
 router.get('/Badminton', function (req, res, next) {
 
-	var td = Date.now();
-	if( moment(lastUpdate).add(2,"minutes") < moment(td))
-	{
-	lastUpdate = td;
-	updater.updateData();
-	}
+	updater();
     res.render('index.html');
 
 });
 router.get('/Futsal', function (req, res, next) {
 
-	var td = Date.now();
-	if( moment(lastUpdate).add(2,"minutes") < moment(td))
-	{
-	lastUpdate = td;
-	updater.updateData();
-	}
+	updater();
     res.render('index.html');
 
 });
+
+
+updater(force = false)
+{
+	var td = Date.now();
+	if( moment(lastUpdate).add(2,"minutes") < moment(td) || force)
+	{
+	lastUpdate = td;
+	updater.updateData();
+	console.log("** Server Data Updated! **");
+	}
+
+}
 
 module.exports = router;
